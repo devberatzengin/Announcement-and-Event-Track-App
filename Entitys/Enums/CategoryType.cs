5e9a287b9ee216cf7255b595c06ec6cb01d2fe77
Berat Zengin <devberatzengin@gmail.com>
2026-07-10 14:57:38 +0300

    feat(Entity): add Categorytype and UserType enums

---
+++ Entitys/Enums/CategoryType.cs
@@ -0,0 +1,10 @@
+namespace Announcement_and_Event_Track_App.Entitys.Enums;
+
+public enum CategoryType
+{
+    Undefined,
+    Draft,
+    Published,
+    Unpublished,
+    Archived
+}
---
+++ Entitys/Enums/UserType.cs
@@ -0,0 +1,8 @@
+namespace Announcement_and_Event_Track_App.Entitys.Enums;
+
+public enum UserType
+{
+    Unknown,
+    Admin,
+    User
+}
