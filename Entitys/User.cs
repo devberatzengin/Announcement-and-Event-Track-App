d8d0040aa3940b7a53307fb752ff165232f740da
Berat Zengin <devberatzengin@gmail.com>
2026-07-10 16:03:52 +0300

    fix(entity): add properties to user entity

--- Entitys/User.cs
+++ Entitys/User.cs
@@ -11,19 +11,19 @@ public class User
     public Guid Id { get; set; } =  Guid.NewGuid();
     
     [Required(ErrorMessage = "Username is required")]
-    public String Username { get; set; }
+    public String Username { get; set; } = String.Empty;
     
     [Required(ErrorMessage = "Password is required")]
-    public String Password { get; set; }
+    public String Password { get; set; } = String.Empty;
     
     [Required(ErrorMessage =  "Email is required")]
-    public String Email { get; set; }
+    public String Email { get; set; } = String.Empty;
     
     [Required(ErrorMessage = "Confirm Password is required")]
     [MaxLength(20),MinLength(2)]
-    public String FirstName { get; set; }
+    public String FirstName { get; set; } = String.Empty;
     
-    public String LastName { get; set; }
+    public String LastName { get; set; } = String.Empty;
     
-    public String PhoneNumber { get; set; }
+    public String PhoneNumber { get; set; } = String.Empty;
 }
