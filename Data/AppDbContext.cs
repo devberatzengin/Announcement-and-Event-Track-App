be5a5ec678de294790611fe04c7661653dbb4dff
Berat Zengin <devberatzengin@gmail.com>
2026-07-16 09:39:47 +0300

    fix: dbContext update for universal time converting operations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 using Announcement_and_Event_Track_App.Entitys;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Announcement_and_Event_Track_App.Data;
 
@@ -17,6 +18,17 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        
+        var utcConverter = new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),                        // DB'ye yazarken
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc)); // DB'den okurken
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        foreach (var property in entityType.GetProperties())
+            if (property.ClrType == typeof(DateTime))
+                property.SetValueConverter(utcConverter);
+
+        
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
